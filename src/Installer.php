39379c29322195f8bc09a1078b0e48b22581be83
Raymond Chong <mathsgod@yahoo.com>
2023-08-31 14:59:30 +0800

    🐛 修正(composer.json)：在autoload中的psr-4設定中，將路徑分隔符號"\"改為"\/" 🐛 修正(Installer.php)：新增對composer事件的引用，以獲取composer實例 ✨ 功能(Installer.php)：將Light.php複製到根目錄並命名為"light"

--- src/Installer.php
+++ src/Installer.php
@@ -8,7 +8,10 @@ class Installer
     {
         //copy Light.php to root
 
+        $composer = $event->getComposer();
+
         $path = __DIR__ . "/Light.php";
+
 
         file_put_contents("light", file_get_contents($path));
     }

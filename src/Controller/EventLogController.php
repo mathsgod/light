d4a80d8195013d1ef5190443ca756bed82a2139f
Raymond Chong <mathsgod@yahoo.com>
2023-08-31 16:52:15 +0800

    🐛 修正(bin/light)：修正對於不同類型的欄位的GraphQL類型映射 ✨ 功能(Controller/EventLogController.php)：新增EventLogController類別，包含listEventLog方法 ✨ 功能(Model/EventLog.php)：新增EventLog模型類別，包含各個欄位的GraphQL類型映射

---
+++ src/Controller/EventLogController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Light\Controller;
+
+use Light\Model\EventLog;
+use TheCodingMachine\GraphQLite\Annotations\InjectUser;
+use TheCodingMachine\GraphQLite\Annotations\Query;
+use TheCodingMachine\GraphQLite\Annotations\Mutation;
+use TheCodingMachine\GraphQLite\Annotations\Right;
+use TheCodingMachine\GraphQLite\Annotations\Logged;
+
+
+class EventLogController
+{
+    #[Query]
+    #[Logged]
+    /**
+     * @return EventLog[]
+     * @params ?mixed $filters
+     */
+    public function listEventLog($filters = [],  ?string $sort = '', #[InjectUser] \Light\Model\User $user): \R\DB\Query
+    {
+        return EventLog::Query()->filters($filters)->sort($sort);
+    }
+}
---
+++ src/Model/EventLog.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Light\Model;
+
+use TheCodingMachine\GraphQLite\Annotations\Field;
+use TheCodingMachine\GraphQLite\Annotations\MagicField;
+use TheCodingMachine\GraphQLite\Annotations\Type;
+
+#[Type]
+#[MagicField(name: "eventlog_id", outputType: "Int!")]
+#[MagicField(name: "class", outputType: "String")]
+#[MagicField(name: "id", outputType: "Int!")]
+#[MagicField(name: "action", outputType: "String")]
+#[MagicField(name: "source", outputType: "mixed")]
+#[MagicField(name: "target", outputType: "mixed")]
+#[MagicField(name: "remark", outputType: "String")]
+#[MagicField(name: "user_id", outputType: "Int!")]
+#[MagicField(name: "created_time", outputType: "String!")]
+#[MagicField(name: "status", outputType: "Int")]
+#[MagicField(name: "different", outputType: "mixed")]
+class EventLog extends \Light\Model
+{
+}

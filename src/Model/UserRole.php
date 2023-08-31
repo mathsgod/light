3e3416e6dde7c7087734254b0c81d44b36e61699
Raymond Chong <mathsgod@yahoo.com>
2023-08-31 15:34:27 +0800

    🔄 重命名(Light.php)：將檔案從src/Light.php改為bin/light 📦 更新(composer.json)：更新autoload路徑為"Light\": "src/" 📦 更新(composer.json)：更新bin路徑為"bin/light" 🐛 修正(UserRole.php)：修正MagicField的outputType屬性為正確的型別

--- src/Model/UserRole.php
+++ src/Model/UserRole.php
@@ -7,8 +7,8 @@ use TheCodingMachine\GraphQLite\Annotations\MagicField;
 use TheCodingMachine\GraphQLite\Annotations\Type;
 
 #[Type]
-#[MagicField(name: "user_role_id", outputType: "!Int")]
-#[MagicField(name: "user_id", outputType: "!Int")]
+#[MagicField(name: "user_role_id", outputType: "Int!")]
+#[MagicField(name: "user_id", outputType: "Int!")]
 #[MagicField(name: "role", outputType: "String")]
 class UserRole extends \Light\Model
 {

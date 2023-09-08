e5a2ba5e6160ccd3034bf398e17037f7831a4fa6
Raymond <mathsgod@yahoo.com>
2023-09-08 15:55:22 +0800

    🔧 修正(FileSystemController.php)：在fsListFiles方法中新增對檔案類型的過濾功能 ✨ 功能(FileSystemController.php)：新增fsMoveFile方法，用於移動檔案 📁 移動(hello.txt)：將hello.txt檔案從uploads資料夾移動到uploads/a資料夾

--- src/Controller/FileSystemController.php
+++ src/Controller/FileSystemController.php
@@ -42,8 +42,32 @@ class FileSystemController
     /**
      * @return \Light\Type\FS\File[]
      */
-    public function fsListFiles(?string $path = ""): array
+    public function fsListFiles(?string $path = "", ?string $type = null): array
     {
+
+        $TYPES = [
+            "image" => ["jpg", "jpeg", "png", "gif", "svg", "webp", "bmp", "ico"],
+            "video" => ["mp4", "webm", "ogg", "avi", "mov", "flv", "wmv", "mkv"],
+            "audio" => ["mp3", "wav", "ogg", "m4a", "flac", "aac"],
+            "document" => ["pdf", "doc", "docx", "xls", "xlsx", "ppt", "pptx", "txt", "csv", "rtf", "odt", "ods", "odp"],
+            "other" => ["zip", "rar", "tar", "gz", "7z", "bz2", "iso", "dmg", "exe", "apk", "torrent"]
+        ];
+
+
+        if ($type !== null) {
+            if (!isset($TYPES[$type])) throw new \Exception("Invalid type: $type");
+
+            $files = [];
+            foreach ($this->fs->listContents($path, true) as $file) {
+                if (!$file->isFile()) continue;
+                $path = $file->path();
+                $ext = pathinfo($path, PATHINFO_EXTENSION);
+                if (!in_array($ext, $TYPES[$type])) continue;
+                $files[] = new \Light\Type\FS\File($this->fs, $file);
+            }
+            return $files;
+        }
+
         $files = [];
         foreach ($this->fs->listContents($path, false) as $file) {
             if (!$file->isFile()) continue;
@@ -64,5 +88,13 @@ class FileSystemController
             $files[] = new \Light\Type\FS\Folder($this->fs, $dir);
         }
         return $files;
+    }
+
+    #[Mutation]
+    public function fsMoveFile(string $source, string $target): bool
+    {
+        $basename = basename($source);
+        $this->fs->move($source, $target . "/" . $basename);
+        return true;
     }
 }

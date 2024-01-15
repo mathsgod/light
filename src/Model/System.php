341fa8cfed60bfd0261ffb9830584892ae747315
Raymond Chong <mathsgod@yahoo.com>
2024-01-15 17:42:13 +0800

    ✨ 新增(System.php)：新增 getMaxUploadSize 方法以獲取最大上傳檔案大小 🔨 修正(Util.php)：修正 ParseSize 方法以處理檔案大小單位轉換

--- src/Model/System.php
+++ src/Model/System.php
@@ -22,6 +22,27 @@ class System
     }
 
     #[Field]
+    public function getMaxUploadSize(): int
+    {
+        $max_size = -1;
+        if ($max_size < 0) {
+            // Start with post_max_size.
+            $post_max_size = \Light\Util::ParseSize(ini_get('post_max_size'));
+            if ($post_max_size > 0) {
+                $max_size = $post_max_size;
+            }
+
+            // If upload_max_size is less, then reduce. Except if upload_max_size is
+            // zero, which indicates no limit.
+            $upload_max = \Light\Util::ParseSize(ini_get('upload_max_filesize'));
+            if ($upload_max > 0 && $upload_max < $max_size) {
+                $max_size = $upload_max;
+            }
+        }
+        return $max_size;
+    }
+
+    #[Field]
     #[Right("system.storage")]
     public function getDiskUsageSpace(): string
     {
--- src/Util.php
+++ src/Util.php
@@ -4,6 +4,18 @@ namespace Light;
 
 class Util
 {
+    static function ParseSize($size)
+    {
+        $unit = preg_replace('/[^bkmgtpezy]/i', '', $size); // Remove the non-unit characters from the size.
+        $size = preg_replace('/[^0-9\.]/', '', $size); // Remove the non-numeric characters from the size.
+        if ($unit) {
+            // Find the position of the unit in the ordered string which is the power of magnitude to multiply a kilobyte by.
+            return round($size * pow(1024, stripos('bkmgtpezy', $unit[0])));
+        } else {
+            return round($size);
+        }
+    }
+
     static function Size($size, array $options = null)
     {
 

c03409b6811bccf809b229d38548f6f8a34b96cf
Raymond Chong <mathsgod@yahoo.com>
2026-01-05 11:54:30 +0800

    ✨ feat(FileSystemController): 新增檔案系統操作功能
    * 新增 `createFolder`、`deleteFolder` 和 `renameFolder` 方法以支援資料夾操作。
    * 在 `writeFile` 方法中加入檔案存在檢查，並提供重新命名功能。
    * 新增 `getNextFilename` 方法以生成不重複的檔案名稱。

--- src/Controller/FileSystemController.php
+++ src/Controller/FileSystemController.php
@@ -3,6 +3,7 @@
 namespace Light\Controller;
 
 use GraphQL\Error\Error;
+use League\Flysystem\MountManager;
 use League\Flysystem\UnixVisibility\PortableVisibilityConverter;
 use Light\App;
 use Light\Model\EventLog;
@@ -20,6 +21,81 @@ use TheCodingMachine\GraphQLite\Annotations\Autowire;
 
 class FileSystemController
 {
+
+    #[Mutation(name: "lightFSCreateFolder")]
+    #[Right("fs.folder:create")]
+    public function createFolder(#[Autowire] MountManager $mountManager, string $location): bool
+    {
+        $mountManager->createDirectory($location);
+        return true;
+    }
+
+    #[Mutation(name: "lightFSDeleteFolder")]
+    #[Right("fs.folder:delete")]
+    public function deleteFolder(#[Autowire] MountManager $mountManager, string $location): bool
+    {
+        $mountManager->deleteDirectory($location);
+        return true;
+    }
+
+    #[Mutation(name: "lightFSRenameFolder")]
+    #[Right("fs.folder:rename")]
+    public function renameFolder(#[Autowire] MountManager $mountManager, string $location, string $newName): bool
+    {
+        $pathParts = pathinfo($location);
+        $dirname = $pathParts['dirname'];
+        if (str_ends_with($dirname, ':')) {
+            $dirname .= '/';
+        }
+
+        $newLocation = $dirname . '/' . $newName;
+        $mountManager->move($location, $newLocation);
+        return true;
+    }
+
+    #[Mutation(name: "lightFSWriteFile")]
+    #[Right("fs.file:write")]
+    public function writeFile(#[Autowire] MountManager $mountManager, string $location, UploadedFileInterface $file, bool $rename = false): string
+    {
+        $pathParts = pathinfo($location);
+        $dirname = $pathParts['dirname'];
+        if (str_ends_with($dirname, ':')) {
+            $dirname .= '/';
+        }
+        $filename = $pathParts['basename'];
+
+        //check if file already exists
+        if ($mountManager->fileExists($location)) {
+
+            if ($rename) {
+                $filename = $this->getNextFilename($mountManager, $dirname, $filename);
+                $location = $dirname . '/' . $filename;
+            } else {
+                throw new Error("File already exists");
+            }
+        }
+
+        //move file
+        $mountManager->write($location, $file->getStream()->getContents());
+
+        return $location;
+    }
+
+    private function getNextFilename(MountManager $mountManager, string $path, string $filename): string
+    {
+        $pathParts = pathinfo($filename);
+        $name = $pathParts['filename'];
+        $extension = isset($pathParts['extension']) ? '.' . $pathParts['extension'] : '';
+        $i = 1;
+        do {
+            $newFilename = $name . ' (' . $i . ')' . $extension;
+            $i++;
+        } while ($mountManager->fileExists($path . '/' . $newFilename));
+        return $newFilename;
+    }
+
+
+
 
     #[Mutation(name: "lightFSupdate")]
     #[Right("filesystem.update")]
